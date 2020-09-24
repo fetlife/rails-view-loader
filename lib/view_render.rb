bf3cd72b2c2375475524343b11b7391880fbd4d5
Andrii Dmytrenko <andrii@fetlife.com>
2020-09-24 14:04:00 +0100

    Use render template

--- lib/view_render.rb
+++ lib/view_render.rb
@@ -17,13 +17,13 @@ query = URI.decode_www_form(url.query.to_s).to_h
 
 lang = query['lang'] || url.path.split('.')[1..-1].join('.')
 
-tmp_file = Tempfile.new(["tmp_view", lang])
+tmp_file = Tempfile.new(["tmp_view", ".#{lang}"])
 tmp_file.write(source)
 tmp_file.flush
 
 layout  ||= query['layout'] || false
 variant ||= query['variant']
 
-output = ApplicationController.render(file: File.basename(tmp_file.path), layout: layout, variant: variant)
+output = ApplicationController.render(template: File.basename(tmp_file.path), layout: layout, variant: variant)
 tmp_file.unlink
 puts "#{delimiter}#{output}#{delimiter}"
