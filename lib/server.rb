a809a05de2dd7b479a37114a8a40649286ba5d02
Andrii Dmytrenko <andrii@fetlife.com>
2020-10-12 17:27:07 +0100

    Switch to ActionController::Metal instead of Sinatra

--- lib/server.rb
+++ lib/server.rb
@@ -1,28 +1,16 @@
-require 'sinatra/base'
 require 'rack/handler/puma'
 require 'tmpdir'
 
-ApplicationController.append_view_path(Dir.tmpdir)
+class RailsViewTemplateRendering < ActionController::Metal
+  include AbstractController::Rendering
+  include ActionView::Layouts
 
-class RailsViewLoaderServer < Sinatra::Base
-  configure :production, :development do
-    enable :logging
-    set(:show_exceptions, false)
-  end
-
-  error do
-    content_type :json
-    e = env['sinatra.error']
-    {:result => 'error', :message => e.message}.to_json
-  end
-
-  post '/' do
-    request.body.rewind  # in case someone already read it
-    options = JSON.parse(request.body.read)
-    resource  = options['resource']
-    layout    = options['layout']
-    variant   = options['variant']
-    source    = options['source']
+  def index
+    append_view_path(Dir.tmpdir)
+    resource  = params['resource']
+    layout    = params['layout']
+    variant   = params['variant']
+    source    = params['source']
 
     url   = URI.parse(resource)
     query = URI.decode_www_form(url.query.to_s).to_h
@@ -32,12 +20,16 @@ class RailsViewLoaderServer < Sinatra::Base
     layout  ||= query['layout'] || false
     variant ||= query['variant']
 
-    output = ApplicationController.render(inline: source, type: lang, layout: layout, variant: variant)
-    output
+    render(
+      inline: source,
+      type: lang,
+      layout: layout,
+      variant: variant,
+    )
   end
 end
 
 port = ARGV[0] || "4567"
 host = ARGV[1] || "127.0.0.1"
 
-Rack::Handler::Puma.run(RailsViewLoaderServer, { Host: host, Port: port })
+Rack::Handler::Puma.run(RailsViewTemplateRendering.action(:index), { Host: host, Port: port })
